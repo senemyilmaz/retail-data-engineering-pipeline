349f225c7ba039b177efc080fbe59c8fd5b2364c
senemyilmaaz@outlook.com <senemyilmaaz@outlook.com>
2026-03-04 12:48:18 +0000

    feat: create staging model for raw product data

---
+++ models/staging/raw/stg_raw__product.sql
@@ -0,0 +1,21 @@
+-- stg_raw__product.sql
+
+with source as (
+
+    select *
+    from {{ source('raw', 'product') }}
+
+),
+
+renamed as (
+
+    select
+        products_id as product_id,
+        cast(purchse_price as float64) as purchase_price
+
+    from source
+
+)
+
+select *
+from renamed
